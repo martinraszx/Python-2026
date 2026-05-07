89bea36aeb65c9855e8d999a06f3aff6fb6fd0a8
martinraszx <martinraszx@gmail.com>
2026-05-07 14:55:13 -0400

    Actualizacion

--- 01-variables.py
+++ 01-variables.py
@@ -1,7 +1,24 @@
+#Se añadieron mas opciones para mostrar en pantalla 
 #Se definen las varialbes a usar
 nombre = "Martin"
 apellido = "Aguirre"
 edad = "18"
 
+"""
+{} Llaves
+() Parentesis
+[] Llaves Cuadradas o Corchetes
+"""
+
 #El escribir las variables
+#Forma uno de mostrar el contenido
 print("Mi nombre y apellido es",nombre, apellido,"y mi edad es", edad)
+
+#Forma dos para mostrar contenido
+print(f"Mi nombre es {nombre} y mi apellido es {apellido} y mi edad es {edad}")
+
+#Froma tres para mostrar contenido
+print("Mi nombre es " + nombre + " y  mi apellido es "+ apellido + " y tengo "+ str(edad) + " edad")
+
+carrera = str(input("¿Que carrera de estudios cursas?"))
+print(f"Yo estudio {carrera}")
---
+++ 02-tiposdedatos.py
@@ -0,0 +1,7 @@
+#Datos númericos
+
+#Numeros Enteros = int
+edad = 18
+
+#Nuemros Reales = float
+estatura = 1.72
