eee1f95f556c67381ad62c4dbaa29f135b4984de
martinraszx <martinraszx@gmail.com>
2026-05-07 14:25:24 -0400

    prueba

---
+++ 01-variables.py
@@ -0,0 +1,7 @@
+#Se definen las varialbes a usar
+nombre = "Martin"
+apellido = "Aguirre"
+edad = "18"
+
+#El escribir las variables
+print("Mi nombre y apellido es",nombre, apellido,"y mi edad es", edad)
