a1c1873bb6326a9f97e169cb7308808578e908cf
martinraszx <martinraszx@gmail.com>
2026-05-12 17:00:59 -0400

    Archivo de listas

---
+++ 03-listas.py
@@ -0,0 +1,11 @@
+#Listas
+
+#Primera forma de Declaracion de Lista Mixta
+lista1=["victor",2,True,4,"victor"]
+
+#Segunda forma de Declaracion de Lista Numerica
+lista2=list([1,2,3,4,5])
+
+#Metodo para las listas
+#01- COUNT()- cuenta el numero de veces que se repite un elemento en la lista
+print(lista1.count("victor"))
